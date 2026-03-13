8b874c7dc23a36073d9fbbf8c0ff302e994e94ef
Mujtaba <mujtabarasheed20@gmail.com>
2026-03-14 03:35:48 +0500

    update

--- Notes/frontend/src/App.jsx
+++ Notes/frontend/src/App.jsx
@@ -3,13 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Auth from './pages/Auth'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-    <div>hello</div>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/auth' element={<Auth/>}/>
+    </Routes>
     </>
   )
 }
---
+++ Notes/frontend/src/pages/Auth.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Auth = () => {
+  return (
+    <div className='min-h-screen overflow-hidden bg-white text-black px-8 '>
+        <header className='max-w-6x1 mx-auto mt-8 rounded-2x1 bg-black/80 border-white/10 px-8 py-6   '>
+
+        </header>
+    </div>
+  )
+}
+
+export default Auth
