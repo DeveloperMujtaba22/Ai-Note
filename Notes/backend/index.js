c5fc1537de4b1f1c01306d6e72813f57689bb307
Mujtaba <mujtabarasheed20@gmail.com>
2026-03-14 02:03:58 +0500

    update

---
+++ Notes/backend/index.js
@@ -0,0 +1,18 @@
+import express from "express"
+import dotenv from "dotenv"
+import connectDB from "./utils/connectDB.js"
+dotenv.config()
+
+
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.get("/",(req,res)=>{
+    res.json({message:"ExamNote Ai backend running 🎆"})
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}`)
+    connectDB()
+})
